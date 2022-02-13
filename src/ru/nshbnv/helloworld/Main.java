b10ff383bd0606394a8468e14809de278ea049d0
Nikita Shabanov <nshbnv@gmail.com>
2022-02-13 11:10:48 +0300

    Refactor output msg

--- src/ru/nshbnv/helloworld/Main.java
+++ src/ru/nshbnv/helloworld/Main.java
@@ -3,6 +3,6 @@ package ru.nshbnv.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, world!");
+	    System.out.println("Bye bye, world!");
     }
 }
