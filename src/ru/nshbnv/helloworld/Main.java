bf20d564131a56bd3307fcfce551c5615d3aa672
Nikita Shabanov <nshbnv@gmail.com>
2022-02-13 13:31:26 +0300

    Added sum

--- src/ru/nshbnv/helloworld/Main.java
+++ src/ru/nshbnv/helloworld/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Bye bye, world!");
+        int a = 1, b = 2;
+        System.out.println(a + b);
     }
 }
