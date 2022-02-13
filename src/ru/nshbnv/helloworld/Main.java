4893c539c3951d74ca149b126910808f617c05b4
Nikita Shabanov <nshbnv@gmail.com>
2022-02-13 13:23:13 +0300

    Added input

--- src/ru/nshbnv/helloworld/Main.java
+++ src/ru/nshbnv/helloworld/Main.java
@@ -1,8 +1,12 @@
 package ru.nshbnv.helloworld;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Bye bye, world!");
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
     }
 }
