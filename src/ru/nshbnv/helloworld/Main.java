967d1a7c80523919a88eef59fd93ed846ca862a3
Nikita Shabanov <nshbnv@gmail.com>
2022-02-15 14:08:32 +0300

    Add output a*b

--- src/ru/nshbnv/helloworld/Main.java
+++ src/ru/nshbnv/helloworld/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Bye bye, world!");
+        System.out.println("Bye bye, world!");
 
         Scanner input = new Scanner(System.in);
         String s = input.next();
 
         int a = 1, b = 2;
         System.out.println(a - b);
+        System.out.println(a * b);
     }
 }
